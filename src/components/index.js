ded9da4831e785b36d05b3371fd478b759af353c
Eric Stéfano <ericssm96@gmail.com>
2025-12-09 20:35:16 -0300

    finished login page base ui

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { FormInputNoLabel } from "./FormInputNoLabel.jsx";
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,20 @@
+import { FormInputNoLabel } from "../components";
+import { UserIcon, LockIcon } from "../components/icons";
+import { Link } from "react-router";
+
 export const Login = () => {
   return (
-    <div>Login</div>
+    <section className="flex h-screen items-center justify-center">
+      <div className="form-container bg-purple-100 rounded-xl w-screen max-w-[420px] p-10 flex flex-col gap-y-4 shadow-lg">
+        <h1 className="text-center text-3xl text-primary">Login</h1>
+        <form className="flex flex-col gap-y-4">
+          <FormInputNoLabel icon={UserIcon} name="email" placeholder="E-mail" type="email" />
+          <FormInputNoLabel icon={LockIcon} name="password" placeholder="Password" type="password" />
+          <button className="btn text-[1rem] btn-lg btn-block btn-primary uppercase">Login</button>
+          <button className="btn text-[1rem] btn-lg btn-block btn-secondary uppercase">Guest User</button>
+        </form>
+        <p className="text-center">Not a member yet? <Link className="text-secondary hover:text-primary" to="/register">Register</Link></p>
+      </div>
+    </section>
   )
 }
