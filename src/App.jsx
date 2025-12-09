cc0484b872dbc71b6f0149a7f71b1fe25c518c9a
Eric Stéfano <ericssm96@gmail.com>
2025-12-09 01:26:36 -0300

    router initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './css/styles.css'
-import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages"
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './css/styles.css';
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from 'react-router/dom';
+import { About, Cart, Checkout, Error, HomeLayout, Landing, Login, Orders, Products, Register, SingleProduct } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <Error />
+      }
+    ]
+  }
+])
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-2xl bg-sky-500 hover:bg-sky-700 inline-block">testing</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
