fe172044e32276b58fb8da358d11f2bc04855e33
Eric Stéfano <ericssm96@gmail.com>
2025-12-10 13:28:26 -0300

    finished ui for register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { Form, Link } from "react-router";
 export const Login = () => {
   return (
     <section className="flex h-screen items-center justify-center">
-      <div className="form-container bg-purple-100 rounded-xl w-screen max-w-[420px] p-10 flex flex-col gap-y-4 shadow-lg">
+      <div className="form-container card w-96 p-10 flex flex-col gap-y-4 shadow-lg">
         <h1 className="text-center text-3xl font-bold text-primary">Login</h1>
         <Form method="POST" className="flex flex-col gap-y-4">
           <FormInputNoLabel icon={UserIcon} defaultValue="test@test.com" name="identifier" placeholder="E-mail" type="email" />
@@ -13,7 +13,7 @@ export const Login = () => {
           <SubmitButton colorClass="btn-primary" text="Login" />
           <button type="button" className="btn text-[1rem] btn-lg btn-block btn-secondary uppercase">Guest User</button>
         </Form>
-        <p className="text-center">Not a member yet? <Link className="text-secondary hover:text-primary" to="/register">Register</Link></p>
+        <p className="text-center">Not a member yet? <Link className="ml-2 link link-hover link-secondary capitalize" to="/register">Register</Link></p>
       </div>
     </section>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,19 @@
+import { LabeledInput, SubmitButton } from "../components";
+import { Form, Link } from "react-router";
+
 export const Register = () => {
   return (
-    <div>Register</div>
+    <section className="flex h-screen items-center justify-center">
+      <div className="form-container card w-96 p-10 flex flex-col gap-y-4 shadow-lg">
+        <h1 className="text-center text-3xl font-bold text-primary">Register</h1>
+        <Form method="POST" className="flex flex-col gap-y-4">
+          <LabeledInput type="text" elemId="username" labelText="Username" name="username" />
+          <LabeledInput type="email" elemId="email" labelText="Email" name="identifier" />
+          <LabeledInput type="password" elemId="password" labelText="Password" name="password" />
+          <SubmitButton colorClass="btn-primary" text="Register" />
+        </Form>
+        <p className="text-center">Already a member? <Link className="ml-2 link link-hover link-secondary capitalize" to="/login">Login</Link></p>
+      </div>
+    </section>
   )
 }
