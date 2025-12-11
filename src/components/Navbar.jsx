668287d4b02d5fb1fa199a0fe3b0f5181c13393e
Eric Stéfano <ericssm96@gmail.com>
2025-12-10 21:03:39 -0300

    navbar initial ui implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,33 @@ export const Navbar = () => {
     <nav className="bg-base-200">
       <div className="navbar align-element">
         <div className="navbar-start">
-          
+          <NavLink className="hidden lg:flex btn btn-primary text-2xl items-center">
+            Comfy
+          </NavLink>
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden" htmlFor="">
+              <FaBarsStaggered className='h-6 w-6' />
+            </label>
+            <ul tabIndex={-1} className="menu menu-sm dropdown-content bg-base-200 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </div>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal">nav links</ul>
         </div>
         <div className="navbar-end">
-          
+          {/* THEME SELECTOR */}
+          {/* CART ITEMS AMMOUNT */}
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+            <div className="indicator">
+              <BsCart3 className='h-6 w-6' />
+              <span className="badge badge-sm badge-primary indicator-item">
+                0
+              </span>
+            </div>
+          </NavLink>
         </div>
       </div>
     </nav>
