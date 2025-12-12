809ff650865d835b7c7c074464991198225a2049
Eric Stéfano <ericssm96@gmail.com>
2025-12-11 22:20:17 -0300

    about page complete

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,23 @@
 export const About = () => {
   return (
-    <div>About</div>
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 justify-center items-center tracking-tight">
+        <h1 className="text-4xl sm:text-6xl font-bold leading-none tracking-tight">
+          We love
+
+          
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, dolorum numquam eos corporis sit, in quos esse magni quasi voluptates veritatis deleniti accusantium id laboriosam blanditiis non ducimus dolores perferendis. Ad nemo quibusdam quas exercitationem quisquam atque laboriosam, rerum amet?
+      </p>
+    </>
   )
 }
