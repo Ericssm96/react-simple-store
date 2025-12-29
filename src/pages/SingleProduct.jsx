82014b19ed0cbe241939b7a968d593f84c9dbb4f
Eric Stéfano <ericssm96@gmail.com>
2025-12-29 20:39:55 -0300

    finished rendering colors element

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -37,12 +37,25 @@ export const SingleProduct = () => {
         </ul>
       </div>
       <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+      {/* IMAGE */}
         <img src={image} alt={title} className="w-96 h-96 object-cover rounded-lg lg:w-full" />
+        {/* PRODUCT INFO */}
         <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
           <h4 className="text-xl text-neutral-content font-bold mt-2">{company}</h4>
           <p className="mt-3 text-xl">{dollarsAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
+          {/* COLORS */}
+          <div className="mt-6">
+            <h4 className="text-md font-medium tracking-wider">Colors</h4>
+            <div className="mt-2">
+              {colors.map((color)=>{
+                return (
+                  <button key={color} type="button" onClick={()=>setProductColor(color)} style={{backgroundColor: color}} className={`badge w-6 h-6 mr-2 ${color === productColor && "border-2 border-secondary"}`}></button>
+                )
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </section>
