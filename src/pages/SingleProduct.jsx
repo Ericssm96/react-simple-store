a4e3a614c8c884ae533a8627e9ec63de79cf6977
Eric Stéfano <ericssm96@gmail.com>
2025-12-29 20:23:44 -0300

    product data rendering partially complete

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -19,8 +19,32 @@ export const SingleProduct = () => {
   const {productData} = useLoaderData();
   const {image, title, price, description, colors, company} = productData.attributes;
   const dollarsAmount = formatPrice(price);
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
-    <div>SingleProduct</div>
+    <section>
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            {title}
+          </li>
+        </ul>
+      </div>
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <img src={image} alt={title} className="w-96 h-96 object-cover rounded-lg lg:w-full" />
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="text-xl text-neutral-content font-bold mt-2">{company}</h4>
+          <p className="mt-3 text-xl">{dollarsAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+        </div>
+      </div>
+    </section>
   )
 }
