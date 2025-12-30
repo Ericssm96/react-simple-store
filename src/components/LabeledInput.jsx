8912bd1013cc9d26454032c18873776016eb225b
Eric Stéfano <ericssm96@gmail.com>
2025-12-30 18:25:55 -0300

    introduced new prop to LabeledInput component to make it more reusable

--- src/components/LabeledInput.jsx
+++ src/components/LabeledInput.jsx
@@ -1,9 +1,9 @@
-export const LabeledInput = ({type, elemId, labelText, name}) => {
+export const LabeledInput = ({type, elemId, labelText, name, size = "md"}) => {
 
   return (
     <div className="input-area flex flex-col items-start gap-y-2">
       <label className="text-sm" htmlFor={elemId}>{labelText}</label>
-      <input type={type} id={elemId} name={name} placeholder="Type here" className="input w-full" />
+      <input type={type} id={elemId} name={name} placeholder="Type here" className={`input w-full ${size}`} />
     </div>
   )
 }
