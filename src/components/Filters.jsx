53f9b381e62bd2feb5dbccb9573c03c4bf7bfe30
Eric Stéfano <ericssm96@gmail.com>
2025-12-30 19:08:09 -0300

    filters component initial setup

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,5 +1,16 @@
+import { Form, useLoaderData, Link } from "react-router";
+import { LabeledInput } from "./LabeledInput";
+
 export const Filters = () => {
   return (
-    <div>Filters</div>
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <LabeledInput type="search" size="sm" labelText="Search Product" name="search" elemId="searchIpt" />
+      {/* BUTTONS */}
+      <button type="submit" className="btn btn-primary btn-sm">
+        Search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">Reset</Link>
+    </Form>
   )
 }
