c7ca1d734724f57f1184e608f47f6566806a0de6
Eric Stéfano <ericssm96@gmail.com>
2025-12-31 15:55:46 -0300

    added loading dots globally

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,20 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import { Header, Navbar } from "../components";
+import { Loading } from "../components";
 
 export const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
+
   return (
     <>
       <Header />
       <Navbar />
-      <main className="align-element py-20">
+      {isPageLoading ? 
+      <Loading /> : 
+      (<main className="align-element py-20">
         <Outlet />
-      </main>
+      </main>)}     
     </>
   )
 }
