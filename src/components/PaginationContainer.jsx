a74f0d41ccf433674d84ca39977ad0442fe2e991
Eric Stéfano <ericssm96@gmail.com>
2026-01-01 19:36:13 -0300

    pagination behavior complete

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -5,44 +5,53 @@ export const PaginationContainer = () => {
   const { productsMeta } = useLoaderData();
   const { pageCount, page } = productsMeta.pagination;
 
+  if(pageCount < 2) return null;
+
+  const { search, pathname } = useLocation();
+  // search shows the query strings of the page we're currently in, if there's any
+  // pathname shows the url path used to access this page. in this case it's "/products"
+  const navigate = useNavigate();
+
   const pages = Array.from({length: pageCount}, (_, index) => {
     return index + 1;
   });
 
-  if(pageCount < 2) return null;
-
   const handlePageChange = (pageNumber) => {
-    console.log(pageNumber);
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
   }
 
   return (
     <div className="mt-16 flex justify-end">
       <div className="join">
+        {page > 1 && (
         <button 
-        onClick={handlePageChange("prev")}
-        className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
+          onClick={handlePageChange(page - 1)}
+          className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
         >
           <BsChevronBarLeft />
-        </button>
+        </button>)}
         {
           pages.map((pageNumber) => {
             return (
               <button 
-              key={pageNumber}
-              onClick={handlePageChange(pageNumber)}
-              className={`btn btn-xs join-item sm:btn-md lg:text-[1.2rem] ${pageNumber == page && "btn-active"}`}
+                key={pageNumber}
+                onClick={()=>handlePageChange(pageNumber)}
+                className={`btn btn-xs join-item sm:btn-md lg:text-[1.2rem] ${pageNumber == page && "btn-active"}`}
               >
                 {pageNumber}
               </button>
             )
           })
         }
+        {page < pages.length && (
         <button 
-        onClick={handlePageChange("next")}
-        className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
+          onClick={()=>handlePageChange(page + 1)}
+          className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
         >
           <BsChevronBarRight />
-        </button>
+        </button>)}
       </div>
     </div>
   )
