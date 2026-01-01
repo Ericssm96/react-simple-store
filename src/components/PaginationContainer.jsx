ad2e813ab6c8a45c56b99a06761943bfea7b35b4
Eric Stéfano <ericssm96@gmail.com>
2026-01-01 03:01:28 -0300

    implemented pagination component. todo: finish behavior

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,49 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router";
+import { BsChevronBarLeft, BsChevronBarRight } from "react-icons/bs";
+
 export const PaginationContainer = () => {
+  const { productsMeta } = useLoaderData();
+  const { pageCount, page } = productsMeta.pagination;
+
+  const pages = Array.from({length: pageCount}, (_, index) => {
+    return index + 1;
+  });
+
+  if(pageCount < 2) return null;
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber);
+  }
+
   return (
-    <div>PaginationContainer</div>
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button 
+        onClick={handlePageChange("prev")}
+        className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
+        >
+          <BsChevronBarLeft />
+        </button>
+        {
+          pages.map((pageNumber) => {
+            return (
+              <button 
+              key={pageNumber}
+              onClick={handlePageChange(pageNumber)}
+              className={`btn btn-xs join-item sm:btn-md lg:text-[1.2rem] ${pageNumber == page && "btn-active"}`}
+              >
+                {pageNumber}
+              </button>
+            )
+          })
+        }
+        <button 
+        onClick={handlePageChange("next")}
+        className="btn btn-xs join-item sm:btn-md lg:text-[1.2rem]"
+        >
+          <BsChevronBarRight />
+        </button>
+      </div>
+    </div>
   )
 }
